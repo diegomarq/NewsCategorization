2dc0583c7f847010ea3b3d69d20e955ace728b24
diegomarq <diegomarques.azevedo@gmail.com>
2017-09-17 21:45:59 -0300

    running with python 3.6 from anaconda path

--- pyspark_intro.py
+++ pyspark_intro.py
@@ -6,17 +6,10 @@ Created on Thu Sep 14 15:14:06
 From: https://spark.apache.org/docs/latest/ml-features.html
 From: https://www.nodalpoint.com/spark-data-frames-from-csv-files-handling-headers-column-types/
 
+Run with: PYSPARK_PYTHON=/home/diego/anaconda3/bin/python3.6 pyspark
+
 @author: diego
 """
-#Upload libriries
-from pyspark.sql import SQLContext
-from pyspark.sql.types import *
-
-import unicodedata
-import string
-
-# Context
-sqlContext = SQLContext(sc)
 
 # Upoad File into data frame
 news_df = spark.read.format("csv").option("header", "true").option("inferSchema", "true").option("delimiter", ";").load("/home/diego/Documents/Data/cod_clas_con_res_tit_semIne.csv")
@@ -38,8 +31,6 @@ news_df = news_df.withColumn("classificacao", trim(news_df.classificacao))
 news_df = news_df.withColumn("conteudo", trim(news_df.conteudo))
 news_df = news_df.withColumn("resumo", trim(news_df.resumo))
 news_df = news_df.withColumn("titulo", trim(news_df.titulo))
-
-
 	
 # Clean everything inside <>
 from pyspark.sql.functions import *    
@@ -63,6 +54,55 @@ remove_accents = udf(lambda c: remove_accents_(c) if c != None else c, StringTyp
 ##############################
 
 news_df = news_df.withColumn('classificacao', remove_accents(news_df.classificacao))
+news_df = news_df.withColumn('conteudo', remove_accents(news_df.conteudo))
+news_df = news_df.withColumn('resumo', remove_accents(news_df.resumo))
+news_df = news_df.withColumn('titulo', remove_accents(news_df.titulo))
+
+# Group and count the categories present in the news
+news_df.groupBy('classificacao').count().sort(col("count").desc()).show()
+
+#+----------------+------+                                                       
+#|   classificacao| count|
+#+----------------+------+
+#|        Politica|189087|
+#|      Judiciario|115388|
+#|        Economia| 93847|
+#|           Saude| 63867|
+#|        Educacao| 38408|
+#|       Violencia| 29791|
+#|   Meio Ambiente| 21106|
+#|     Agronegocio| 20108|
+#|         Esporte| 15714|
+#|         Energia| 15210|
+#|      Transporte| 14581|
+#|  Infraestrutura| 13867|
+#|        Trabalho| 12768|
+#|         Cultura| 11170|
+#|       Habitacao|  6217|
+#|    Politica GDF|  6148|
+#|         Aviacao|  6102|
+#| Contas Publicas|  5431|
+#|Telecomunicacoes|  5331|
+#|      Legislacao|  5293|
+#+----------------+------+
+#only showing top 20 rows
+
+##############################
+# Tokenizer dataframe column
+import numpy
+from pyspark.ml.feature import Tokenizer, RegexTokenizer
+from pyspark.sql.functions import col, udf
+from pyspark.sql.types import IntegerType
+
+tokenizer = Tokenizer(inputCol="resumo", outputCol="token_resumo")
+
+countTokens = udf(lambda words: len(words), IntegerType())
+
+tokenized = tokenizer.transform(news_df)
+tokenized.select("resumo", "token_resumo").withColumn("num_token_resumo", 
+                countTokens(col("token_resumo"))).show(truncate=False)
+
+
 
 
 #newsFile = sc.textFile("file:///home/diego/Documents/Data/cod_clas_con_res_tit_semIne.csv")
