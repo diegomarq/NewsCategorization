8459adf844adadff591efc15208a01af29f9934e
diegomarq <diegomarques.azevedo@gmail.com>
2017-09-19 01:32:36 -0300

    more of data understanding

--- pyspark_intro.py
+++ pyspark_intro.py
@@ -14,7 +14,7 @@ Run with: PYSPARK_PYTHON=/home/diego/anaconda3/bin/python3.6 pyspark
 # Upoad File into data frame
 news_df = spark.read.format("csv").option("header", "true").option("inferSchema", "true").option("delimiter", ";").load("/home/diego/Documents/Data/cod_clas_con_res_tit_semIne.csv")
 
-news_df.printSchema()
+#news_df.printSchema()
 #root
 # |-- codigo: integer (nullable = true)
 # |-- classificacao: string (nullable = true)
@@ -22,7 +22,7 @@ news_df.printSchema()
 # |-- resumo: string (nullable = true)
 # |-- titulo: string (nullable = true)
 
-news_df.count()
+#news_df.count()
 # 714415
 
 # Remove extra spaces
@@ -50,7 +50,34 @@ def remove_accents_(inputStr):
     return withOutAccents
 
 remove_accents = udf(lambda c: remove_accents_(c) if c != None else c, StringType())
-    
+
+# Null categories
+from pyspark.sql.functions import isnan, when, count, col
+
+news_df.select([count(when(isnan(c) | col(c).isNull(), c)).alias(c) for c in news_df.columns]).show()
+#
+#+------+-------------+--------+------+------+                                   
+#|codigo|classificacao|conteudo|resumo|titulo|
+#+------+-------------+--------+------+------+
+#|     0|            0|     127|  4753|     3|
+#+------+-------------+--------+------+------+
+
+# column conteudo with empty value
+news_df[news_df.conteudo == ''].count()
+#4750
+
+# Get only conteudo not empty
+news_df = news_df.where(news_df.conteudo != '')
+
+# Now    
+#
+#+------+-------------+--------+------+------+                                   
+#|codigo|classificacao|conteudo|resumo|titulo|
+#+------+-------------+--------+------+------+
+#|     0|            0|       0|     2|     3|
+#+------+-------------+--------+------+------+
+
+
 ##############################
 
 news_df = news_df.withColumn('classificacao', remove_accents(news_df.classificacao))
@@ -60,47 +87,85 @@ news_df = news_df.withColumn('titulo', remove_accents(news_df.titulo))
 
 # Group and count the categories present in the news
 news_df.groupBy('classificacao').count().sort(col("count").desc()).show()
-
+#
 #+----------------+------+                                                       
 #|   classificacao| count|
 #+----------------+------+
-#|        Politica|189087|
-#|      Judiciario|115388|
-#|        Economia| 93847|
-#|           Saude| 63867|
-#|        Educacao| 38408|
-#|       Violencia| 29791|
-#|   Meio Ambiente| 21106|
-#|     Agronegocio| 20108|
-#|         Esporte| 15714|
-#|         Energia| 15210|
-#|      Transporte| 14581|
-#|  Infraestrutura| 13867|
-#|        Trabalho| 12768|
-#|         Cultura| 11170|
-#|       Habitacao|  6217|
-#|    Politica GDF|  6148|
-#|         Aviacao|  6102|
-#| Contas Publicas|  5431|
-#|Telecomunicacoes|  5331|
-#|      Legislacao|  5293|
+#|        Politica|187803|
+#|      Judiciario|114835|
+#|        Economia| 93441|
+#|           Saude| 63747|
+#|        Educacao| 38337|
+#|       Violencia| 29667|
+#|   Meio Ambiente| 20939|
+#|     Agronegocio| 20053|
+#|         Esporte| 15672|
+#|      Transporte| 14276|
+#|         Energia| 13987|
+#|  Infraestrutura| 13608|
+#|        Trabalho| 12736|
+#|         Cultura| 11146|
+#|    Politica GDF|  6146|
+#|       Habitacao|  6146|
+#|         Aviacao|  6082|
+#| Contas Publicas|  5419|
+#|Telecomunicacoes|  5329|
+#|     Previdencia|  5290|
 #+----------------+------+
+
 #only showing top 20 rows
+
+news_df.groupBy('classificacao').count().sort(col("count").asc()).show()
+#
+#+-----------------+-----+                                                       
+#|    classificacao|count|
+#+-----------------+-----+
+#|         Producao|  529|
+#|        Mineracao|  751|
+#|         Especial| 1970|
+#|          Credito| 2062|
+#|Comercio Exterior| 2477|
+#|          Consumo| 2926|
+#|         Petroleo| 4237|
+#|  Turismo e lazer| 4641|
+#|       Legislacao| 5286|
+#|      Previdencia| 5290|
+#| Telecomunicacoes| 5329|
+#|  Contas Publicas| 5419|
+#|          Aviacao| 6082|
+#|     Politica GDF| 6146|
+#|        Habitacao| 6146|
+#|          Cultura|11146|
+#|         Trabalho|12736|
+#|   Infraestrutura|13608|
+#|          Energia|13987|
+#|       Transporte|14276|
+#+-----------------+-----+
+#only showing top 20 rows
+
+
+cla_count_asc_df = news_df.groupBy('classificacao').count().sort(col("count").asc())
+
+# Number of classifcations or categories
+cla_count_asc_df.count()
+#29                          
+
+
 
 ##############################
 # Tokenizer dataframe column
-import numpy
-from pyspark.ml.feature import Tokenizer, RegexTokenizer
-from pyspark.sql.functions import col, udf
-from pyspark.sql.types import IntegerType
-
-tokenizer = Tokenizer(inputCol="resumo", outputCol="token_resumo")
-
-countTokens = udf(lambda words: len(words), IntegerType())
-
-tokenized = tokenizer.transform(news_df)
-tokenized.select("resumo", "token_resumo").withColumn("num_token_resumo", 
-                countTokens(col("token_resumo"))).show(truncate=False)
+#import numpy
+#from pyspark.ml.feature import Tokenizer, RegexTokenizer
+#from pyspark.sql.functions import col, udf
+#from pyspark.sql.types import IntegerType
+#
+#tokenizer = Tokenizer(inputCol="resumo", outputCol="token_resumo")
+#
+#countTokens = udf(lambda words: len(words), IntegerType())
+#
+#tokenized = tokenizer.transform(news_df)
+#tokenized.select("resumo", "token_resumo").withColumn("num_token_resumo", 
+#                countTokens(col("token_resumo"))).show(truncate=False)
 
 
 
